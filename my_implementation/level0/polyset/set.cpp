a75fdbbdf27a80efdc91957283662872a137e995
aitsim <oussamatachakourt@gmail.com>
2026-06-05 12:56:44 +0100

    still

---
+++ my_implementation/level0/polyset/set.cpp
@@ -0,0 +1,64 @@
+
+#include "set.hpp"
+
+set::set() : _bag(0)
+{
+}
+
+set::set(searchable_bag &bag) : _bag(&bag)
+{
+}
+
+set::set(const set &other) : _bag(other._bag)
+{
+}
+
+set &set::operator=(const set &other)
+{
+	if (this != &other)
+		_bag = other._bag;
+	return *this;
+}
+
+set::~set()
+{
+}
+
+bool set::has(int value) const
+{
+	if (!_bag)
+		return false;
+	return _bag->has(value);
+}
+
+void set::insert(int value)
+{
+	if (_bag && !_bag->has(value))
+		_bag->insert(value);
+}
+
+void set::insert(int *array, int size)
+{
+	if (!_bag || !array)
+		return;
+
+	for (int i = 0; i < size; i++)
+		insert(array[i]);
+}
+
+void set::print() const
+{
+	if (_bag)
+		_bag->print();
+}
+
+void set::clear()
+{
+	if (_bag)
+		_bag->clear();
+}
+
+const searchable_bag &set::get_bag() const
+{
+	return *_bag;
+}
---
+++ my_implementation/level0/polyset/set.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "searchable_bag.hpp"
+
+class set : public searchable_bag
+{
+private:
+	searchable_bag *_bag;
+
+public:
+	set();
+	set(searchable_bag &bag);
+	set(const set &other);
+	set &operator=(const set &other);
+	~set();
+
+	bool has(int value) const;
+	void insert(int value);
+	void insert(int *array, int size);
+	void print() const;
+	void clear();
+
+	const searchable_bag &get_bag() const;
+};
