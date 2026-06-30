bce25fb3b8beecefcdc11459159bb98b8053d191
oussama7007 <oussamatachakourt@gmail.com>
2026-06-30 23:10:01 +0100

    still

--- level01/life/life.c
+++ level01/life/life.c
@@ -1,58 +1,67 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-
+#include <stdio.h>
 
 int main(int ac, char **av)
 {
-    if(ac != 4)
+    if (ac != 4)
         return 0;
+
     int w = atoi(av[1]), h = atoi(av[2]), iter = atoi(av[3]);
-    if(w <= 0 || h <= 0)
+    if (w <= 0 || h <= 0)
         return 0;
 
-    char board[h][w], tmp[h][w], c;
-    int x = 0, y = 0, p = 0;
-    
-    memset(board, 0, sizeof(board));
+    char *board = calloc(h * w, sizeof(char));
+    char *tmp   = calloc(h * w, sizeof(char));
+    if (!board || !tmp)
+        return 1;
 
-    while(read(0, &c, 1) > 0)
+    int px = 0, py = 0, pen = 0;
+    char c;
+
+    while (read(0, &c, 1) > 0)
     {
-        if(c == 'w' && y > 0) y--;
-        if(c == 's' && y < h - 1) y++;
-        if(c == 'a' && x > 0) x--;
-        if(c == 'd' && x < w - 1) x++;
-        if(c == 'x') p = !p;
-        if(p) board[y][x] = 1;
+        if (c == 'w' && py > 0)     py--;
+        if (c == 's' && py < h - 1) py++;
+        if (c == 'a' && px > 0)     px--;
+        if (c == 'd' && px < w - 1) px++;
+        if (c == 'x')               pen = !pen;
+        if (pen)                    board[py * w + px] = 1;
     }
 
-    while(iter-- > 0)
+    while (iter-- > 0)
     {
-        for(int y = 0; y < h; y++)
+        for (int row = 0; row < h; row++)
         {
-            for(int x = 0; x < w; x++)
+            for (int col = 0; col < w; col++)
             {
                 int n = 0;
-                for(int dy = -1; dy <= 1; dy++)
-                {
-                    for(int dx = -1; dx <= 1; dx++)
-                    {
-                        if((dy != 0 || dx != 0) && y + dy >= 0 && y + dy < h && x + dx >= 0 && x + dx < w)
-                            n += board[y+dy][x+dx];
-                    }
-                }
-                tmp[y][x] = (board[y][x] && n == 2) || (n == 3); 
+                for (int dy = -1; dy <= 1; dy++)
+                    for (int dx = -1; dx <= 1; dx++)
+                        if ((dy || dx) && row + dy >= 0 && row + dy < h && col + dx >= 0 && col + dx < w)
+                            n += board[(row + dy) * w + (col + dx)];
+                tmp[row * w + col] = (board[row * w + col] && n == 2) || (n == 3);
             }
         }
-        memcpy(board, tmp, sizeof(board));
+        for (int i = 0; i < h * w; i++)
+            board[i] = tmp[i];
     }
 
-    for(int i = 0; i < h; i++)
-    {
-        for(int j = 0; j < w; j++)
-            putchar(board[i][j] ? '0' : ' ');
-        putchar('\n');
-    }
+
+for (int row = 0; row < h; row++)
+{
+    int alive = 0;
+    for (int col = 0; col < w; col++)
+        if (board[row * w + col]) { alive = 1; break; }
+
+    if (alive)
+        for (int col = 0; col < w; col++)
+            putchar(board[row * w + col] ? 'O' : ' ');
+
+    putchar('\n');
+}
+    free(board);
+    free(tmp);
     return 0;
 }
+
